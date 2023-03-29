af61169d090e2a44ce9c5be2cf341832abd1384d
anishgithub890 <anishgithub890@gmail.com>
2023-03-29 16:18:39 +0545

    file-manage-1

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { motion } from "framer-motion";
-
-import { images } from "../../constants";
-import "./Header.scss";
-import { Helmet } from "react-helmet-async";
-import AppWrap_Social from "../../wrapper/AppWrap_Social";
-import { MotionWrap } from "../../wrapper";
+import React from 'react';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
+import './Header.scss';
+import { Helmet } from 'react-helmet-async';
+import AppWrap_Social from '../../wrapper/AppWrap_Social';
+import { MotionWrap } from '../../wrapper';
 
 const scaleVariants = {
   whileInView: {
@@ -13,70 +12,73 @@ const scaleVariants = {
     opacity: [0, 1],
     transition: {
       duration: 1,
-      ease: "easeInOut",
+      ease: 'easeInOut',
     },
   },
 };
 
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <>
       <Helmet>
         <title>HOME</title>
       </Helmet>
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="app__header-info"
-      >
-        <div className="app__header-badge">
-          <div className="badge-cmp app__flex">
-            <span>👋</span>
-            <div style={{ marginLeft: 20 }}>
-              <p className="p-text">Hello, I am</p>
-              <h1 className="head-text">Anish</h1>
+
+      <div className="app__header app__flex">
+        <motion.div
+          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="app__header-info"
+        >
+          <div className="app__header-badge">
+            <div className="badge-cmp app__flex">
+              <span>👋</span>
+              <div style={{ marginLeft: 20 }}>
+                <p className="p-text">Hello, I am</p>
+                <h1 className="head-text">Anish</h1>
+              </div>
+            </div>
+            <div className="tag-cmp app__flex">
+              <p className="p-text">Mern Stack Developer</p>
+              <p className="p-text">Freelancer</p>
             </div>
           </div>
-          <div className="tag-cmp app__flex">
-            <p className="p-text">Mern Stack Developer</p>
-            <p className="p-text">Freelancer</p>
-          </div>
-        </div>
-      </motion.div>
+        </motion.div>
 
-      <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        <img src={images.profile} alt="profile_bg" />
+        <motion.div
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
+          className="app__header-img"
+        >
+          <img src={images.profile} alt="profile_bg" />
 
-        <motion.img
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: "easeInOut" }}
-          src={images.circle}
-          alt="profile_circle"
-          className="overlay_circle"
-        />
-      </motion.div>
+          <motion.img
+            whileInView={{ scale: [0, 1] }}
+            transition={{ duration: 1, ease: 'easeInOut' }}
+            src={images.circle}
+            alt="profile_circle"
+            className="overlay_circle"
+          />
+        </motion.div>
 
-      <motion.div
-        variants={scaleVariants}
-        whileInView={scaleVariants.whileInView}
-        className="app__header-circles"
-      >
-        {[images.m, images.react, images.nodejs].map((circle, index) => (
-          <div className="circle-cmp app__flex" key={`circle-${index}`}>
-            <img src={circle} alt="circle" />
-          </div>
-        ))}
-      </motion.div>
-    </div>
+        <motion.div
+          variants={scaleVariants}
+          whileInView={scaleVariants.whileInView}
+          className="app__header-circles"
+        >
+          {[images.m, images.react, images.nodejs].map((circle, index) => (
+            <div className="circle-cmp app__flex" key={`circle-${index}`}>
+              <img src={circle} alt="circle" />
+            </div>
+          ))}
+        </motion.div>
+      </div>
+    </>
   );
 };
 
 export default AppWrap_Social(
-  MotionWrap(Header, "home"),
-  "about",
-  "app__whitebg"
+  MotionWrap(Header, 'home'),
+  'about',
+  'app__whitebg'
 );
